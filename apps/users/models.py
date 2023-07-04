ef03c99175e3d0da0c4e0f6ea25201dbb80695da
Toktorov <ktoktorov2005@gmail.com>
2023-07-04 15:24:13 +0600

    start project

---
+++ apps/users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+
+# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=20, validators=[MinLengthValidator(3)],
+        verbose_name="Имя пользователя", unique=True,
+        error_messages={
+            "unique": ("A user with that username already exists."),
+        },
+    )
+
+    def __str__(self):
+        return self.username 
+    
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
